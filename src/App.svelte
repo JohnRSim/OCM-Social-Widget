e75ea8b51b9b1ec9b3b282b14828dd74f38c05f8
John Sim <jsim@fishbowlsolutions.com>
2022-09-14 09:51:24 +0100

    add ui interactions

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,11 @@
 <script>
 	import icoSearch from './assets/ico_search.svg';
 	import icoArrow from './assets/ico_arrow.svg';
+
+	//components
+	import HeaderTabList from './lib/components/HeaderTabList.svelte';
+
+	let searchActive = false;
 </script>
 
 <!-- Wrapper -->
@@ -15,8 +20,13 @@
 				<!-- xLabel -->
 
 				<!-- Search Field -->
-				<div class="userSearchField" style="background-image:url({icoSearch})">
-					<input id="socialUserSearch" type="search" placeholder="Search Network Users..." />
+				<div class="userSearchField" class:active="{searchActive}" style="background-image:url({icoSearch})">
+					<input 
+						id="socialUserSearch" 
+						type="search" 
+						placeholder="Search Network Users..." 
+						on:focus="{() => {searchActive = true;}}"
+						on:blur="{() => {searchActive = false;}}"/>
 					<button style="background-image:url({icoArrow})"></button>
 				</div>
 				<!-- xSearch Field -->
@@ -26,10 +36,21 @@
 
 		<!-- Tabs-->
 		<nav>
-			<ul>
-				<li>Following</li>
-				<li>Follower</li>
-			</ul>
+			<HeaderTabList
+				on:tab="{(e) => {
+					//goto(e.detail.path, { replaceState: true, noscroll: true });
+				}}"
+				hasTabs="{[
+					{
+						name: 'Following',
+						path: `/?tab=Tokens`,
+					},
+					{
+						name: 'Followers',
+						path: `/?tab=NFTs`,
+					},
+				]}"
+				activeTab="Following" />
 		</nav>
 		<!-- Tabs-->
 
@@ -217,8 +238,8 @@
 
 <style>
 	article {
-		border-radius:20px;
-		box-shadow:0px 2px 4px  0px rgba(0,0,0,0.3);
+		border-radius:26px 26px 20px 20px;
+		box-shadow:0px 2px 4px  0px rgba(0,0,0,0.15);
 		min-width:400px;
 		min-height:100px;
 		overflow: hidden;
@@ -231,14 +252,20 @@
 	.userSearchField {
 		display: flex;
 		background:#F9FAFC;
-		box-shadow:inset 0px 0px 1px 0px rgba(0,0,0,0.3);
+		box-shadow:inset 0px 0px 2px 0px rgba(0,0,0,0.1);
 		border-radius: 100px;
-		padding-left:40px;
+		padding-left:48px;
 		background-repeat: no-repeat;
-		background-size:20px;
+		background-size:26px;
 		background-position:  10px center;
+		transition: box-shadow 0.3s, background 0.3s;
 	}
 	
+	.userSearchField:hover,
+	.userSearchField.active {
+		background-color:#fff;
+		box-shadow:inset 0px 0px 2px 0px rgba(0,0,0,0.4),0px 1px 2px 0px rgba(0,0,0,0.1);
+	}
 	.userSearchField ::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
 		color: #687889;
 		font-weight: 600;
@@ -247,14 +274,15 @@
 	input {
 		border:0px;
 		background:0px;
-		height:40px;
+		height:48px;
 		font-weight:500;
 		flex:1;
+		font-size:1.05em
 	}
 
 	button {
-		width:40px;
-		height:40px;
+		width:48px;
+		height:48px;
 		border-radius: 50%;
 		background: #8F9BA8;
 		box-shadow:inset 0px 0px 0px 3px #7C8B9A;
@@ -263,15 +291,24 @@
 		background-size:30px;
 		background-position:  center;
 		cursor: pointer;
+		/*transition: box-shadow 0.3s;*/
+		transition: background 0.3s;
 	}
+	
+	button:hover {
+		/*box-shadow:inset 0px 0px 0px 50px #7C8B9A;*/
+		background-color: #7C8B9A;
+	}
+
 	fieldset {
 		border:0px;
-		padding:6px 4px;
+		padding:6px 6px 10px;
+		margin:0px;
 	}
 	nav {
 		border-top:solid 0.5px #F5F5F5;
 		border-bottom:solid 0.5px #F5F5F5;
-		padding:6px;
+		padding:6px 12px;
 	}
 
 	main ul {
@@ -331,5 +368,7 @@
 	background: #7C8B9A; 
 	border-radius: 100px;
 }
+
+
 
 </style>
